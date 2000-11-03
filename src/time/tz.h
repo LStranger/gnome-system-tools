28b247ca2f7e05577282a718e6f3a104d4e9636b
Hans Petter <hansp@src.gnome.org>
2000-11-03 18:37:09 +0000

    Updated descriptions.

--- src/time/tz.h
+++ src/time/tz.h
@@ -1,7 +1,26 @@
-/*  GNOME canvas based interface to a map using a simple cylindrical proj */
-/*                                                                        */
-/* Copyright (C) 1999 Red Hat, Incorportated                              */
-/* Original work by Michael Fulbright <drmike@redhat.com> */
+/* Generic timezone utilities.
+ *
+ * Copyright (C) 2000 Helix Code, Inc.
+ *
+ * Authors: Hans Petter Jansson <hpj@helixcode.com>
+ * 
+ * Largely based on Michael Fulbright's work on Anaconda.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 
 #ifndef _E_TZ_H
 #define _E_TZ_H
