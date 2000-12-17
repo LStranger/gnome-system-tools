65dfa22d90bfb11f01f00923bc11a0c5e033472c
Arturo Espinosa <unammx@src.gnome.org>
2000-12-17 08:35:15 +0000

    Handle general element sensitivity.

--- src/users/main.c
+++ src/users/main.c
@@ -41,6 +41,30 @@
 #include "e-table.h"
 #include "callbacks.h"
 
+static void set_access_sensitivity (void)
+{
+	char *access_no[] = {"user_new", "user_chpasswd", "group_new", 
+		                   "user_settings_basic", "user_settings_advanced",
+		                   "group_settings_name_label", "group_settings_add", 
+		                   "group_settings_remove", NULL};
+	char *access_yes[] = {"user_list_sw", "group_list_sw", NULL};
+	char *unsensitive[] = {"user_delete", "user_settings", "user_chpasswd", "group_delete",
+		                     "group_settings", NULL};
+	int i;
+	
+	/* Those widgets that won't be available if you don't have the access. */
+	for (i = 0; access_no[i]; i++)
+		gtk_widget_set_sensitive (tool_widget_get (access_no[i]), tool_get_access());
+	
+	/* Those widgets that will be available, even if you don't have the access. */
+	for (i = 0; access_yes[i]; i++)
+		gtk_widget_set_sensitive (tool_widget_get (access_yes[i]), TRUE);
+	
+	/* Those widgets you should never have access to, and will be activated later on. */
+	for (i = 0; unsensitive[i]; i++)
+		gtk_widget_set_sensitive (tool_widget_get (unsensitive[i]), FALSE);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -58,6 +82,7 @@ main (int argc, char *argv[])
 
   
 	gtk_widget_show (tool_get_top_window ());
+	set_access_sensitivity ();
 	gtk_main ();
 
 	return 0;
