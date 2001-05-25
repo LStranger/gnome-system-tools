391c335669f15e574bc44745d8be395d6a389606
Hans Petter Jansson <hpj@ximian.com>
2001-05-25 23:08:28 +0000

    Introduced a short delay, so the frontend will have time to write the
    2001-05-25  Hans Petter Jansson  <hpj@ximian.com>
    
    	* xst-su.c (exec_su): Introduced a short delay, so the frontend will have
    	  time to write the password to su(1).

--- src/common/xst-su.c
+++ src/common/xst-su.c
@@ -162,6 +162,7 @@ exec_su (gchar *exec_path, gchar *user, gchar *pwd)
 		freopen ("/dev/null", "w", stderr);
 		freopen ("/dev/null", "w", stdout);
 
+		sleep (1);
 		execlp ("su", "su", "-c", exec_p, user_p, NULL);
 
 		_exit (0);
