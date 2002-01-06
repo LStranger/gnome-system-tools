c6ba1706f5b44e6b13e68c1c9de03ab8ff4e7ee8
Jose Maria Celorio <chema@src.gnome.org>
2002-01-06 23:23:53 +0000

    make sure the user has src/dhcpd or else ask him to cvs -z3 co

--- autogen.sh
+++ autogen.sh
@@ -6,9 +6,13 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="the package."
 
-(test -f $srcdir/configure.in) || {
+(test -f $srcdir/configure.in \
+ && test -f $srcdir/src/dhcpd/dhcpd.h) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level directory"
+    echo ""
+    echo "Perhaps you need to \"cvs -z3 co ximian-setup-tools\" in \"..\" ?"
+    echo
     exit 1
 }
 
