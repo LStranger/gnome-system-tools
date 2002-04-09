492913fb21e9d2d9b58973161270131cfa24a7a1
Tambet Ingo <tambet@ximian.com>
2002-04-09 07:27:17 +0000

    Fixes for gnome2.
    2002-04-09  Tambet Ingo  <tambet@ximian.com>
    
    	* boot.glade.in: Fixes for gnome2.
    
    	* common.glade.in: Updated pixmaps locations.
    
    	* network.glade.in Fixes for gnome2.

--- interfaces/boot.glade.h
+++ interfaces/boot.glade.h
@@ -9,8 +9,8 @@ gchar *s = N_("Use this tool to set the installed operating\n"
               "systems to choose from at startup.");
 gchar *s = N_("_Add...");
 gchar *s = N_("_Delete");
-gchar *s = N_("_Settings...");
-gchar *s = N_("D_efault");
+gchar *s = N_("_Edit...");
+gchar *s = N_("Set as D_efault");
 gchar *s = N_("Starting your computer");
 gchar *s = N_("_Wait");
 gchar *s = N_("_seconds before loading default kernel or OS");
@@ -25,9 +25,9 @@ gchar *s = N_("_Root:");
 gchar *s = N_("_Append:");
 gchar *s = N_("Other");
 gchar *s = N_("_Device:");
-gchar *s = N_("Boot Image Druid");
-gchar *s = N_("Boot Image Druid");
-gchar *s = N_("This druid is designed to help you quickly \n"
+gchar *s = N_("Boot Image Assistant");
+gchar *s = N_("Boot Image Assistant");
+gchar *s = N_("This assistant is designed to help you quickly \n"
               "and easily add new boot images to your system.\n"
               "\n"
               "To begin, click \"Next\". ");
@@ -35,7 +35,7 @@ gchar *s = N_("Image Identity (step 1 of 2)");
 gchar *s = N_("Enter a name for the boot image, and specify what kind of operating system it is.");
 gchar *s = N_("Click \"Next\" to continue");
 gchar *s = N_("Other  (step 2 of 2)");
-gchar *s = N_("Specify the partition the the operating system is installed on.");
+gchar *s = N_("Specify the partition on which the operating system is installed.");
 gchar *s = N_("Click \"Next\" to continue");
 gchar *s = N_("Image (step 2 of 2)");
 gchar *s = N_("Choose a kernel image to boot, and a partition to mount as the root partition.\n"
