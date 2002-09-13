510912cf6d35287106e6eef4ba191a9b1e92be9a
Tambet Ingo <tambet@ximian.com>
2002-09-13 10:00:51 +0000

    Converted all .glade files to glade-2.
    2002-09-13  Tambet Ingo  <tambet@ximian.com>
    
    	* Makefile.am: Converted all .glade files to glade-2.

--- interfaces/common.glade.h
+++ interfaces/common.glade.h
@@ -7,12 +7,10 @@
 gchar *s = N_("GNOME System Tools");
 gchar *s = N_("Scanning your system configuration.");
 gchar *s = N_("GNOME System Tools");
-gchar *s = N_("Complexity");
 gchar *s = N_("Unsupported platform");
 gchar *s = N_("The platform you are running is not supported by this tool. If you know for sure that it works like one of the platforms listed below, you can select that and continue.\n"
               "\n"
               "Note, however, that this might damage the system configuration or downright cripple your computer.");
-gchar *s = N_("label57");
 gchar *s = N_("GNOME System Tools - Password");
 gchar *s = N_("Run without password");
 gchar *s = N_("To run this tool, you need administrator privileges.\n"
@@ -20,3 +18,4 @@ gchar *s = N_("To run this tool, you need administrator privileges.\n"
               "To acquire such privileges, enter your root password below. If you don't\n"
               "specify a password, you will be unable to modify your system configuration.");
 gchar *s = N_("Password:");
+gchar *s = N_("*");
