0e997420c96ecd7f6727f285ef09ed93d919d348
Carlos Garnacho Parro <garparr@teleline.es>
2002-09-14 15:40:17 +0000

    Made common.glade.* HIG compliant
    2002-09-14  Carlos Garnacho Parro  <garparr@teleline.es>
    
    	* Made common.glade.* HIG compliant

--- interfaces/common.glade.h
+++ interfaces/common.glade.h
@@ -11,12 +11,12 @@ gchar *s = N_("The platform you are running is not supported by this tool. If yo
               "\n"
               "Note, however, that this might damage the system configuration or downright cripple your computer.");
 gchar *s = N_("GNOME System Tools - Password");
-gchar *s = N_("Run without password");
-gchar *s = N_("To run this tool, you need administrator privileges.\n"
+gchar *s = N_("<span weight=\"bold\">To run this tool, you need administrator privileges.</span>\n"
               "\n"
               "To acquire such privileges, enter your root password below. If you don't\n"
               "specify a password, you will be unable to modify your system configuration.");
 gchar *s = N_("Password:");
 gchar *s = N_("*");
+gchar *s = N_("Run without password");
 gchar *s = N_("Gnome System Tools");
 gchar *s = N_("Complexity");
