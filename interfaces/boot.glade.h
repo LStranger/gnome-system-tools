84c3be379f3c0fcf1811bb2ad180df772d16c354
Carlos Garnacho Parro <garnacho@tuxerver.net>
2002-11-19 23:20:25 +0000

    removed ToggleButton to make unsensitive boot timeout, it has no sense if
    2002-11-20  Carlos Garnacho Parro <garnacho@tuxerver.net>
    
            * boot.glade.*: removed ToggleButton to make unsensitive boot timeout,
              it has no sense if the timeout can be set to 0.

--- interfaces/boot.glade.h
+++ interfaces/boot.glade.h
@@ -12,6 +12,7 @@ gchar *s = N_("_Delete");
 gchar *s = N_("_Edit...");
 gchar *s = N_("Set as D_efault");
 gchar *s = N_("_Wait");
+gchar *s = N_("Wait");
 gchar *s = N_("_seconds before loading default kernel or OS");
 gchar *s = N_("Starting your computer");
 gchar *s = N_("window2");
