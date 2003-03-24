d23fca647f9cad42b6be715bd26ac602ebd87231
Carlos Garnacho Parro <garnacho@tuxerver.net>
2003-03-24 23:23:36 +0000

    removed the accelerator from the "Time zone" label, it isn't neccesary.
    2003-03-24  Carlos Garnacho Parro  <garnacho@tuxerver.net>
    
             * time.glade.*: removed the accelerator from the "Time zone" label,
             it isn't neccesary. Fixes bug #108518

--- interfaces/time.glade.h
+++ interfaces/time.glade.h
@@ -14,7 +14,7 @@ gchar *s = N_("window1");
 gchar *s = N_("Please select the current date and time. You can also specify a time zone and a time server.");
 gchar *s = N_("<span weight=\"bold\">Time Configuration</span>");
 gchar *s = N_("_Time:");
-gchar *s = N_("Time _zone:");
+gchar *s = N_("Time zone:");
 gchar *s = N_(":");
 gchar *s = N_(":");
 gchar *s = N_("(unconfigured)");
