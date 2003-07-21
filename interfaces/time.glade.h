1e89b1ef5278bc989b498280bbcd1d3c8c37dec3
Christian Neumair <chris@gnome-de.org>
2003-07-21 12:35:29 +0000

    Some adaptions to make the beast more HIG-compliant.
    2003-07-21  Christian Neumair  <chris@gnome-de.org>
    
    	* common.glade.*, network.glade.*, runlevel.glade.*, time.glade.*:
    	Some adaptions to make the beast more HIG-compliant.

--- interfaces/time.glade.h
+++ interfaces/time.glade.h
@@ -1,11 +1,11 @@
 /*
- * Archivo de cadenas traducibles generado por Glade.
- * Añada este archivo a su POTFILES.in de su proyecto.
- * NO lo compile como parte de su aplicación.
+ * Datei für übersetzbare Zeichenketten, erzeugt durch Glade.
+ * Fügen Sie diese Datei zur POTFILES.in Ihres Projektes hinzu.
+ * Compilieren Sie sie NICHT als Teil Ihrer Anwendung.
  */
 
 gchar *s = N_("To select a time zone, click on one of the dots on the map");
-gchar *s = N_("Time Zone:");
+gchar *s = N_("Time zone:");
 gchar *s = N_("*");
 gchar *s = N_("Select the servers you wish to use for time synchronization.");
 gchar *s = N_("*");
@@ -20,4 +20,4 @@ gchar *s = N_(":");
 gchar *s = N_("Se_lect Servers...");
 gchar *s = N_("_Select Time Zone...");
 gchar *s = N_("(unconfigured)");
-gchar *s = N_("S_ynchronize clock with Internet servers");
+gchar *s = N_("S_ynchronize clock with Internet servers:");
