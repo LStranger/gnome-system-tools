b34f8337fe003d4ad9e1069e4c5c913723b64189
Carlos Garnacho Parro <garnacho@tuxerver.net>
2004-05-30 12:02:48 +0000

    removed unused code that depended on the deprecated GtkOptionMenu widget
    2004-05-30  Carlos Garnacho Parro  <garnacho@tuxerver.net>
    
            * gst-ui.[ch]: removed unused code that depended on the deprecated
              GtkOptionMenu widget

--- src/common/gst-ui.h
+++ src/common/gst-ui.h
@@ -5,18 +5,10 @@
 #ifndef GST_UI_H
 #define GST_UI_H
 
-void       gst_ui_combo_remove_by_label           (GtkCombo *combo, const gchar *label);
 GtkWidget *gst_ui_image_widget_create             (gchar *name, gchar *string1, gchar *string2,
 						   gint int1, gint int2);
 void       gst_ui_image_set_pix                   (GtkWidget *widget, gchar *filename);
 GtkWidget *gst_ui_image_widget_get                (GladeXML *gui, gchar *name);
-
-gint       gst_ui_option_menu_get_selected_row    (GtkOptionMenu *option_menu);
-gchar     *gst_ui_option_menu_get_selected_string (GtkOptionMenu *option_menu);
-void       gst_ui_option_menu_set_selected_string (GtkOptionMenu *option_menu, const gchar *string);
-GtkWidget *gst_ui_option_menu_add_string          (GtkOptionMenu *option_menu, const gchar *string);
-void       gst_ui_option_menu_remove_string       (GtkOptionMenu *option_menu, const gchar *string);
-void       gst_ui_option_menu_clear               (GtkOptionMenu *option_menu);
 
 void       gst_ui_entry_set_text                  (void *entry, const gchar *str);
 
