52728f4aa2e909b3bf7fe79abcc15a02caf39ade
Joe Marcus Clarke <marcus@freebsd.org>
2004-08-24 13:56:40 +0000

    fixes for calling correctly the backend both in Linux and FreeBSD (using
    2004-08-24  Joe Marcus Clarke  <marcus@freebsd.org>
    
            * gst-auth.c: fixes for calling correctly the backend both in Linux
            and FreeBSD (using env, not export). Fixes #150693

--- src/common/gst-auth.c
+++ src/common/gst-auth.c
@@ -300,14 +300,16 @@ gst_auth_save_locale (GString* command) {
 	lc_all      = getenv ("LC_ALL");
 	lang        = getenv ("LANG");
 
+	g_string_append (command, "env ");
+
 	if (lc_all)
-		g_string_append_printf (command, "export LC_ALL=\"%s\" && ", lc_all);
-	else if (lc_messages)
-		g_string_append_printf (command, "export LC_MESSAGES=\"%s\" && ", lc_messages);
-	else if (lang)
-		g_string_append_printf (command, "export LANG=\"%s\" && ", lang);
-	else if (language)
-		g_string_append_printf (command, "export LANGUAGE=\"%s\" && ", language);
+		g_string_append_printf (command, "LC_ALL=\"%s\" ", lc_all);
+	if (lc_messages)
+		g_string_append_printf (command, "LC_MESSAGES=\"%s\" ", lc_messages);
+	if (lang)
+		g_string_append_printf (command, "LANG=\"%s\" ", lang);
+	if (language)
+		g_string_append_printf (command, "LANGUAGE=\"%s\" ", language);
 }
 
 void
