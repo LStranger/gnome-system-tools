b9cb7859e2a2ff59dad986c3c3342b1440a06344
Carlos Garnacho <carlosg@gnome.org>
2006-09-07 11:36:18 +0000

    Added a bunch of icons to the descriptions list.
    2006-09-07  Carlos Garnacho  <carlosg@gnome.org>
    
            * service.c: Added a bunch of icons to the descriptions list.

--- src/services/service.c
+++ src/services/service.c
@@ -44,25 +44,25 @@ const ServiceDescription descriptions[] = {
 	{ FALSE, "clock",              N_("Clock synchronization service"),   N_("Synchronizes your computer "
 										 "clock with Internet time servers") },         /* GST_ROLE_NTP_SERVER */
 	{ FALSE, "stock_lock",         N_("Antivirus"),                       N_("Analyzes your incoming mail for virus") },    /* GST_ROLE_ANTIVIRUS */
-	{ FALSE, NULL,                 N_("Firewall"),                        N_("Blocks undesired network "
+	{ FALSE, "stock_lock",         N_("Firewall"),                        N_("Blocks undesired network "
 										 "access to your computer") },                  /* GST_ROLE_FIREWALL_MANAGEMENT */
 	{ FALSE, NULL,                 N_("Dictionary server"),               NULL }, /* GST_ROLE_DICTIONARY_SERVER */
-	{ FALSE, NULL,                 N_("Speech synthesis support"),        NULL }, /* GST_ROLE_SPEECH_SYNTHESIS */
+	{ FALSE, "access",             N_("Speech synthesis support"),        NULL }, /* GST_ROLE_SPEECH_SYNTHESIS */
 	{ FALSE, "logviewer",          N_("Computer activity logger"),        N_("Keeps a log of your computer activity") },    /* GST_ROLE_SYSTEM_LOGGER */
 	{ FALSE, NULL,                 N_("Remote backup server"),            NULL }, /* GST_ROLE_REMOTE_BACKUP */
 	{ FALSE, "stock_spam",         N_("Spam filter"),                     NULL }, /* GST_ROLE_SPAM_FILTER */
 	{ FALSE, "gnome-terminal",     N_("Remote shell server"),             N_("Secure shell server") },                      /* GST_ROLE_SECURE_SHELL_SERVER */
-	{ FALSE, NULL,                 N_("Application server"),              NULL }, /* GST_ROLE_APPLICATION_SERVER */
+	{ FALSE, "stock_script",       N_("Application server"),              NULL }, /* GST_ROLE_APPLICATION_SERVER */
 	{ FALSE, NULL,                 N_("Automated crash reports support"), NULL }, /* GST_ROLE_AUTOMATED_CRASH_REPORTS_MANAGEMENT */
 	{ FALSE, NULL,                 N_("System communication bus"),        NULL }, /* GST_ROLE_DBUS, */
-	{ FALSE, NULL,                 N_("System configuration manager"),    NULL }, /* GST_ROLE_SYSTEM_CONFIGURATION_MANAGEMENT */
+	{ FALSE, "gtk-preferences",    N_("System configuration manager"),    NULL }, /* GST_ROLE_SYSTEM_CONFIGURATION_MANAGEMENT */
 	{ FALSE, NULL,                 N_("School management platform"),      NULL }, /* GST_ROLE_SCHOOL_MANAGEMENT_PLATFORM */
-	{ FALSE, NULL,                 N_("Network security auditor"),        NULL }, /* GST_ROLE_SECURITY_AUDITING */
-	{ FALSE, NULL,                 N_("Web calendar server"),             NULL }, /* GST_ROLE_WEB_CALENDAR_SERVER */
+	{ FALSE, "stock_lock",         N_("Network security auditor"),        NULL }, /* GST_ROLE_SECURITY_AUDITING */
+	{ FALSE, "stock_web-calendar", N_("Web calendar server"),             NULL }, /* GST_ROLE_WEB_CALENDAR_SERVER */
 	{ FALSE, NULL,                 N_("OEM configuration manager"),       NULL }, /* GST_ROLE_OEM_CONFIGURATION_MANAGEMENT */
 	{ FALSE, NULL,                 N_("Terminal multiplexor"),            NULL }, /* GST_ROLE_TERMINAL_MULTIPLEXOR */
 	{ TRUE,  NULL,                 N_("Disk quota activation"),           NULL }, /* GST_ROLE_QUOTA_MANAGEMENT */
-	{ FALSE, NULL,                 N_("Package index monitor"),           NULL }, /* GST_ROLE_PACKAGE_INDEX_MONITORING */
+	{ FALSE, "gnome-package",      N_("Package index monitor"),           NULL }, /* GST_ROLE_PACKAGE_INDEX_MONITORING */
 	{ TRUE,  NULL,                 N_("Network service"),                 NULL }, /* GST_ROLE_NETWORK */
 	{ FALSE, NULL,                 N_("Dynamic DNS services updater"),    NULL }, /* GST_ROLE_DYNAMIC_DNS_SERVICE */
 	{ FALSE, NULL,                 N_("DHCP server"),                     NULL }, /* GST_ROLE_DHCP_SERVER */
@@ -81,18 +81,18 @@ const ServiceDescription descriptions[] = {
 	{ TRUE,  NULL,                 N_("RPC mapper"),                      NULL }, /* GST_ROLE_RPC_MAPPER */
 	{ TRUE,  NULL,                 N_("SNMP server"),                     NULL }, /* GST_ROLE_SNMP_SERVER */
 	{ TRUE,  NULL,                 N_("Terminal server client"),          NULL }, /* GST_ROLE_LTSP_CLIENT */
-	{ FALSE, NULL,                 N_("Audio settings management"),       NULL }, /* GST_ROLE_AUDIO_MANAGEMENT */
+	{ FALSE, "gnome-mixer",        N_("Audio settings management"),       NULL }, /* GST_ROLE_AUDIO_MANAGEMENT */
 	{ FALSE, "disks",              N_("Volumes mounter"),                 N_("Mounts your volumes automatically") },        /* GST_ROLE_AUTOMOUNTER */
 	{ FALSE, NULL,                 N_("Infrared port management"),        NULL }, /* GST_ROLE_INFRARED_MANAGEMENT */
-	{ TRUE,  NULL,                 N_("Braille display management"),      NULL }, /* GST_ROLE_BRAILLE_DISPLAY_MANAGEMENT */
-	{ FALSE, NULL,                 N_("Bluetooth device management"),     NULL }, /* GST_ROLE_BLUETOOTH_MANAGEMENT */
-	{ FALSE, NULL,                 N_("Hard disk tuning"),                NULL }, /* GST_ROLE_HDD_MANAGEMENT */
+	{ TRUE,  "access",             N_("Braille display management"),      NULL }, /* GST_ROLE_BRAILLE_DISPLAY_MANAGEMENT */
+	{ FALSE, "stock_bluetooth",    N_("Bluetooth device management"),     NULL }, /* GST_ROLE_BLUETOOTH_MANAGEMENT */
+	{ FALSE, "drive-harddisk",     N_("Hard disk tuning"),                NULL }, /* GST_ROLE_HDD_MANAGEMENT */
 	{ FALSE, NULL,                 N_("Hotkeys management"),              NULL }, /* GST_ROLE_HOTKEYS_MANAGEMENT */
 	{ FALSE, NULL,                 N_("Power management"),                NULL }, /* GST_ROLE_POWER_MANAGEMENT */
 	{ TRUE,  NULL,                 N_("Logical volume management"),       NULL }, /* GST_ROLE_LVM_MANAGEMENT */
 	{ TRUE,  NULL,                 N_("Cluster management tool"),         NULL }, /* GST_ROLE_CLUSTER_MANAGEMENT */
 	{ FALSE, NULL,                 N_("Fax settings management"),         NULL }, /* GST_ROLE_FAX_MANAGEMENT */
-	{ TRUE,  NULL,                 N_("RAID disks management"),           NULL }, /* GST_ROLE_RAID_MANAGEMENT */
+	{ TRUE,  "drive-harddisk",     N_("RAID disks management"),           NULL }, /* GST_ROLE_RAID_MANAGEMENT */
 	{ FALSE, NULL,                 N_("Graphic tablets management"),      NULL }, /* GST_ROLE_GRAPHIC_TABLETS_MANAGEMENT */
 	{ FALSE, NULL,                 N_("CPU Frequency manager"),           NULL }, /* GST_ROLE_CPUFREQ_MANAGEMENT */
 	{ FALSE, NULL,                 N_("Eagle USB ADSL modems manager"),   NULL }, /* GST_ROLE_EAGLE_USB_MODEMS_MANAGEMENT */
