f6e6fe7a88541ab89ab60817f0d3450383c99624
Carlos Garnacho <carlosg@src.gnome.org>
2007-02-12 15:36:35 +0000

    Doh! obviously a typo
    svn path=/tags/LIBOOBS_2_17_91; revision=3773

--- src/common/gst.h
+++
@@ -1,9 +0,0 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
-#include <config.h>
-#include "gst-types.h"
-#include "gst-widget.h"
-#include "gst-tool.h"
-#include "gst-dialog.h"
-#include "gst-conf.h"
-#include "gst-filter.h"
-#include "gst-service-role.h"
